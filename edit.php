1cdeaef6bd86b05d522e613eb96b723f26d32c55
LauraStefanne <148160006+LauraStefanne@users.noreply.github.com>
2024-06-12 16:30:05 -0300

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@
     if(!empty($_GET['id']))
     {
         $id = $_GET['id'];
-        $sqlSelect = "SELECT * FROM usuarios WHERE id=$id";
+        $sqlSelect = "SELECT * FROM cliente WHERE id=$id";
         $result = $conexao->query($sqlSelect);
         if($result->num_rows > 0)
         {
